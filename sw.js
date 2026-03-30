b67eeb6bf7e02803b2017a79ce1223f719491a85
jlyajure <jlyt1710@gmail.com>
2026-03-30 12:42:08 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Instalado');
-});
-self.addEventListener('fetch', (e) => {
-  // Motor pasivo para permitir instalación
-});
